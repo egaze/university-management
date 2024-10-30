5bea9efda8226b321aae52462e394a6353277829
Eyiram Gaze <eyiramgaze@gmail.com>
2024-10-30 07:17:17 +0000

    created Driver to test

--- src/Driver.java
+++ src/Driver.java
@@ -1,5 +1,6 @@
 public class Driver {
     public static void main(String[] args) {
-        Student student = new Student();
+        Student student = new Student("Eyiram", "Computer Engineering");
+        System.out.println(student);
     }
 }
